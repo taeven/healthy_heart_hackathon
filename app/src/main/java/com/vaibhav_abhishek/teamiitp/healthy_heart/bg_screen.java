624f44eaaaadcd91e6832eb6015a64260be838a1
Abhishek Agrawal <aagrawal207@gmail.com>
2016-10-01 02:25:08 +0530

    Abhishek

--- app/src/main/java/com/vaibhav_abhishek/teamiitp/healthy_heart/bg_screen.java
+++ app/src/main/java/com/vaibhav_abhishek/teamiitp/healthy_heart/bg_screen.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class bg_screen extends AppCompatActivity {
 
